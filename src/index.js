5cd7605e183e36f6ce7069d22171b4ed8dbfb54f
LukeAnger <anger.luke@gmail.com>
2023-01-26 16:27:38 -0500

    Added redux files and first actions/reducers

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { createroot } from 'react-dom/client'
-import App from './App.jsx'
-
-const root = createroot(document.getElementById("root"))
-
-root.render(<App />)
---
+++ src/reducers/allDayReducer.js
@@ -0,0 +1,7 @@
+import { ADD_ITEM, DELETE_ITEM } from '../actions/actionTypes.js'
+
+const initialState = {
+  list: [];
+};
+
+export default const allDayReducer = (state = initialState, )
